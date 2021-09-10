bf8d45370bc0d7f077847dc5b521aa0959119a95
Jaime Ramos <jrossetes@gmail.com>
2021-09-10 10:26:20 -0500

    chore: add cors to allow traffic from anywhere

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	structs "bff-twitter-frontend-class/tweets"
 	http "net/http"
+	time "time"
 
+	cors "github.com/gin-contrib/cors"
 	gin "github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	tweets := structs.GetTweets()
 	router.GET("/tweets", func(c *gin.Context) {
 		c.JSON(http.StatusOK, tweets)
